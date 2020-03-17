bf38b5c65dbf9d0e94c2983e46209e1f6a460819
Jose Victor Persset <victor.persset@gmail.com>
2020-03-16 21:14:53 -0300

    finalizacao aula 5

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { YelowBox, YellowBox } from 'react-native';
 import Routes from './src/routes';
+
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket'
+]);
 
 export default function App() {
   return <Routes />
